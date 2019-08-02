9b8805e19be4bfb557870b61d604191e5e7b6033
Hana <18086006+khana87@users.noreply.github.com>
2019-08-02 22:58:24 +0000

    Add seeds

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,4 +16,8 @@ class SessionsController < ApplicationController
     redirect_to root_url
   end
   
+  private
+    def login_params
+      params.require(:session).permit(:email, :password)
+    end
 end
