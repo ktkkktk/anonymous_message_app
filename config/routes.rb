d85d63d40e368d7f9afdbfeb81cdc8c29402511a
Hana <18086006+khana87@users.noreply.github.com>
2019-08-07 13:42:50 +0000

    Finish to implement a card-form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   post    '/login',   to: "sessions#create"
   delete  '/logout',  to: "sessions#destroy"
-  post    '/card',    to: "message_cards#create"
   resources :users, only: [:show, :create]
   resources :settings, only: :show
+  resources :message_cards, only: [:show, :create, :destroy]
 end
--- test/controllers/message_cards_controller_test.rb
+++ test/controllers/message_cards_controller_test.rb
@@ -2,21 +2,21 @@ require 'test_helper'
 
 class MessageCardsControllerTest < ActionDispatch::IntegrationTest
   test "should send a message to valid user" do
-    user_sent_to = User.first
-    post card_path, params: { message_card: {content: "Good morning", user_id: user_sent_to.id }}
+    user_sent_to = users(:tanaka)
+    post message_cards_path, params: { message_card: {content: "Good morning", user_id: user_sent_to.id }}
     assert_redirected_to root_url
     assert_not flash[:success].empty?
   end
   
   test "should not save an empty message" do
-    user_sent_to = User.first
-    post card_path, params: { message_card: {content: "", user_id: user_sent_to.id }}
+    user_sent_to = users(:tanaka)
+    post message_cards_path, params: { message_card: {content: "", user_id: user_sent_to.id }}
     assert_template 'users/show'
     assert_not flash[:error].empty?
   end
   
   test "should not send a message to invalid user" do
-    post card_path, params: { message_card: {content: "Good evening", user_id: "#{User.count + 1}" }}
+    post message_cards_path, params: { message_card: {content: "Good evening", user_id: "#{User.count + 1}" }}
     assert_redirected_to root_url
     assert_not flash[:error].empty?
   end
